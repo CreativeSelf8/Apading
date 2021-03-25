cd09679cf54d83c805d8796b58719e73a024f0b0
quyennv84 <73773856+quyennv84@users.noreply.github.com>
2021-03-25 12:58:46 +0900

    init

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const path = require('path');
+require('dotenv').config()
+
+const app = express();
+
+const playerRoutes = require('./routes/player.routes');
+const homeRoutes = require('./routes/index.routes');
+
+const { DATABASE, DB_HOST, DB_PORT, DB_USER, DB_PASS, PORT } = process.env;
+
+
+// create connection to database
+// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
+const db = mysql.createConnection ({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DATABASE,
+    port: DB_PORT,
+    insecureAuth: false
+});
+
+// connect to database
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+global.db = db;
+
+// configure middleware
+app.set('port', PORT); // set express to use this port
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse form data client
+app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
+app.use(fileUpload()); // configure fileupload
+
+// routes for the app
+app.use('/', homeRoutes);
+app.use('/player', playerRoutes);
+app.get('*', function(req, res, next){
+    res.status(404);
+
+    res.render('404.ejs', {
+        title: "Page Not Found",
+    });
+
+});
+
+// set the app to listen on the port
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
---
+++ public/assets/js/index.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+
+    $(".list_house").lightSlider({
+        item: 4,
+        autoWidth: true,
+        slideMove: 1,
+        slideMargin: 10,
+        speed: 1000,
+        auto: true,
+        loop: true,
+        slideEndAnimation: true,
+        keyPress: true,
+        controls: true,
+        pager: false,
+        enableTouch: true,
+        enableDrag: true,
+        freeMove: true,
+    });
+
+    $(".list_home_infor_2").lightSlider({
+        item: 1,
+        autoWidth: false,
+        slideMove: 1,
+        slideMargin: 0,
+        speed: 1000,
+        auto: false,
+        loop: true,
+        slideEndAnimation: true,
+        keyPress: true,
+        controls: true,
+        pager: false,
+    });
+
+    $(".list_home_infor_1").lightSlider({
+        item: 1,
+        autoWidth: false,
+        slideMove: 1,
+        slideMargin: 0,
+        speed: 1000,
+        auto: false,
+        loop: true,
+        slideEndAnimation: true,
+        keyPress: true,
+        controls: true,
+        pager: true,
+    });
+
+    $(".slide-furniture").lightSlider({
+        item: 1,
+        autoWidth: false,
+        slideMove: 1,
+        slideMargin: 0,
+        speed: 1000,
+        auto: false,
+        loop: true,
+        slideEndAnimation: true,
+        keyPress: true,
+        controls: true,
+        pager: true,
+    });
+
+    $(".accessory-product").lightSlider({
+        item: 4,
+        auto:true,
+        slideMove: 1,
+        loop:true,
+        pauseOnHover: true,
+        pager: false,
+
+    });
+
+
+
+    if ($(window).width() < 768) {
+        $("#image").lightSlider({
+            item: 1,
+            autoWidth: false,
+            slideMove: 1,
+            slideMargin: 0,
+            speed: 1000,
+            auto: false,
+            loop: true,
+            slideEndAnimation: true,
+            keyPress: true,
+            controls: true,
+            pager: false,
+        });
+        $("#list_video").lightSlider({
+            item: 1,
+            autoWidth: false,
+            slideMove: 1,
+            slideMargin: 0,
+            speed: 1000,
+            auto: false,
+            loop: true,
+            slideEndAnimation: true,
+            keyPress: true,
+            controls: true,
+            pager: false,
+        });
+    }
+
+
+    $('.btn_1').click(() => {
+        $('.btn_1').toggleClass('red');
+        $('.btn_2').removeClass('red');
+    });
+
+    $('.btn_2').click(() => {
+        $('.btn_2').toggleClass('red');
+        $('.btn_1').removeClass('red');
+    });
+
+
+    $('.select').click(() => {
+        $('.dropdown_content').toggleClass('display');
+    });
+
+});
